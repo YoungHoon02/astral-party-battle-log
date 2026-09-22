2557609c526a10fb8ec9e9073a9b893ff6adc425
최영훈 <yh03011@naver.com>
2026-09-23 03:12:45 +0900

    chore: 재접속 개선 사항을 v0.2.0 배포에 반영

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ using HarmonyLib;
 
 namespace AstralPartyBattleLog;
 
-[BepInPlugin(Guid, "Astral Party Battle Log", "0.1.9")]
+[BepInPlugin(Guid, "Astral Party Battle Log", "0.2.0")]
 public class Plugin : BasePlugin
 {
     public const string Guid = "astralparty.battlelog";
