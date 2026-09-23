05dd36aad6e125d7f668d171a7c3a022ef7675fa
최영훈 <yh03011@naver.com>
2026-09-23 20:57:27 +0900

    chore: 라운드 전환 개선판 버전 0.3.1 준비

--- Plugin.cs
+++ Plugin.cs
@@ -12,7 +12,7 @@ using HarmonyLib;
 
 namespace AstralPartyBattleLog;
 
-[BepInPlugin(Guid, "Astral Party Battle Log", "0.3.0")]
+[BepInPlugin(Guid, "Astral Party Battle Log", "0.3.1")]
 public class Plugin : BasePlugin
 {
     public const string Guid = "astralparty.battlelog";
